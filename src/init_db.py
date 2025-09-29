2b13f1f51fac5276620f94938c84482ad9bae566
Tom Theunissen <tomtheunissen2006@gmail.com>
2025-09-29 10:02:15 +0200

    Categorie toegevoegd aan database

--- src/init_db.py
+++ src/init_db.py
@@ -4,10 +4,14 @@ def init_db():
     conn = sqlite3.connect("data/products.db")
     cursor = conn.cursor()
 
+    # Verwijder oude tabel als die nog bestaat
+    cursor.execute("DROP TABLE IF EXISTS producten")
+
     # Maak tabel
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS producten (
+    CREATE TABLE producten (
         code TEXT PRIMARY KEY,
+        categorie TEXT NOT NULL,
         naam TEXT NOT NULL,
         prijs REAL NOT NULL,
         co2_score REAL
@@ -16,13 +20,17 @@ def init_db():
 
     # Voeg testproducten toe
     producten = [
-        ("1001", "Broodje kaas", 2.50, 0.35),
-        ("1002", "Koffie", 1.80, 0.10),
-        ("1003", "Frisdrank", 2.00, 0.50)
+        ("1001", "Broodjes", "Broodje kaas", 2.50, 0.35),
+        ("1002", "Warme dranken", "Koffie", 1.80, 0.10),
+        ("1003", "Koude dranken", "Cola", 2.00, 0.50)
     ]
-    cursor.executemany("INSERT OR IGNORE INTO producten VALUES (?, ?, ?, ?)", producten)
+    cursor.executemany("INSERT OR IGNORE INTO producten VALUES (?, ?, ?, ?, ?)", producten)
 
     conn.commit()
+
+    for row in cursor.execute("SELECT * FROM producten"):
+        print(row)
+
     conn.close()
     print("Database aangemaakt en gevuld!")
 
