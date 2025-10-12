636084788bd567501f95811b443af07aeeacba0e
Tom Theunissen <tomtheunissen2006@gmail.com>
2025-10-12 22:51:21 +0200

    Database uitgebreid

--- src/init_db.py
+++ src/init_db.py
@@ -20,9 +20,17 @@ def init_db():
 
     # Voeg testproducten toe
     producten = [
-        ("1001", "Broodjes", "Broodje kaas", 2.50, 0.35),
-        ("1002", "Warme dranken", "Koffie", 1.80, 0.10),
-        ("1003", "Frisranken", "Cola", 2.00, 0.50)
+        ("1001", "Warme dranken", "Koffie", 1.20, 2),
+        ("1002", "Warme dranken", "Thee", 1.20, 8),
+        ("2001", "Koude dranken", "Cola", 2.00, 5),
+        ("2002", "Koude dranken", "Spa blauw", 1.50, 7),
+        ("2003", "Koude dranken", "Cola", 2.00, 5),
+        ("2004", "Koude dranken", "Ice Tea", 2.00, 5),
+        ("3001", "Broodjes", "Broodje kaas", 2.50, 4),
+        ("3002", "Broodjes", "Broodje Ham", 2.50, 4),
+        ("3003", "Broodjes", "Broodje Gezond", 3.00, 4),
+        ("3004", "Broodjes", "Broodje Gehaktbal", 2.90, 4),
+        ("4001", "Warme gerechten", "Panini Ham & Kaas", 3.20, 4)
     ]
     cursor.executemany("INSERT OR IGNORE INTO producten VALUES (?, ?, ?, ?, ?)", producten)
 
