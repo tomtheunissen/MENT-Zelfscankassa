1546262b9b0970b95a5eb378529decaa2b93dd92
Tom Theunissen <tomtheunissen2006@gmail.com>
2025-10-15 17:11:33 +0200

    comments toegevoegd

--- src/database.py
+++ src/database.py
@@ -1,11 +1,18 @@
-import sqlite3
+import sqlite3 # voor database
 
 def get_product(code):
+    # maak verbinding met database
     conn = sqlite3.connect("data/products.db")
     cursor = conn.cursor()
+
+    # zoek product via productcode
     cursor.execute("SELECT naam, prijs FROM producten WHERE code = ?", (code,))
     row = cursor.fetchone()
+
+    # beëindigt verbinding met database
     conn.close()
+
+    # geef product als dict of None wanneer er niks gevonden is
     if row:
         return {"naam": row[0], "prijs": row[1]}
     return None
